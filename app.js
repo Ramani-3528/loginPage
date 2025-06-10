134139cabfeed5873460b1fe36a60d2e3de248cc
ramani-3528 <ramaneshwari3528@gmail.com>
2025-06-10 22:34:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function login(){
+  const user = document.getElementById("Username").value;
+  const pass = document.getElementById("Password").value;
+
+  if (user === "ramani" && pass === "2343") {
+    document.getElementById("output").innerText="Login successful";
+    window.location.href="./login.html";
+  }
+  else if (user === "" || pass === "") {
+    alert("Please fillout both boxes");
+  } else {
+    alert("Invalid username or password!");
+  }
+}
