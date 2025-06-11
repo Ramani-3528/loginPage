2d0f74699dda13161dffe8504fb678e808714451
ramani-3528 <ramaneshwari3528@gmail.com>
2025-06-11 22:02:00 +0530

    user

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function login(){
   const user = document.getElementById("Username").value;
   const pass = document.getElementById("Password").value;
 
-  if (user === "ramani" && pass === "2343") {
+  if (user === "siva" && pass === "0269") {
     document.getElementById("output");
     window.location.href="./login.html";
   }
